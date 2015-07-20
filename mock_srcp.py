be9a81a39aaf370675d9e053d1a56deee9819a93
ralf <ralfoide@gmail.com>
2015-07-19 19:59:14 -0700

    Mock SRCP multi-thread that works well with RocRail.
    This still fails with JMRI.

--- mock_srcp.py
+++ mock_srcp.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-from socket import *
-
-s = socket(AF_INET, SOCK_STREAM)
-s.bind( ("localhost", 4303) )
-s.listen(5)  # accept up to 5 cnx at the same time
+import socket
+import threading
+import SocketServer
+import time
+import sys
 
 MODE_HANDSHAKE = "HANDSHAKE"
 MODE_INFO      = "INFO"
@@ -13,27 +13,37 @@ MODE_COMMAND   = "COMMAND"
 _client_id = 0
 _sessions = []
 _time = 0
+_continue = True
 
-class Session:
-    def __init__(self, conn):
-        self.conn = conn
-        self.conn.send("RalfMockSvr 1; SRCP 0.8.4; SRCOTHER 0.8.3\n")
+class SessionRequestHandler(SocketServer.BaseRequestHandler):
+    def __init__(self, request, client_address, server):
+        SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
+
+    def setup(self):
         self.mode = MODE_HANDSHAKE
-        self.session_id = 0
-        self.power="OFF"
+        global _sessions
+        _sessions.append(self)
+        self.session_id = len(_sessions)
+        print "Add session", self.session_id
+        self.power = "OFF"
 
     def reply(self, s):
         global _time
         _time += 1
         s = "%ld %s" % (_time, s)
-        print self.session_id, "<",s
-        self.conn.send(s + "\n")
+        print "[%d] <" % self.session_id, s
+        self.request.sendall(s + "\n")
 
     def handle(self):
+        global _sessions
+        # Note for JMRI I seem to have to disable the header, why??
+        header = "SRCP 0.8.4; SRCPOTHER 0.8.3"
+        print "[%d] <" % self.session_id, header
+        self.request.sendall(header + "\n")
+
         while True:
-            c = self.conn.recv(1000)
-            c = c.strip()
-            print self.session_id, ">",c
+            c = self.request.recv(1000).strip()
+            print "[%d] >" % self.session_id, c
             
             # Handshake mode
             if c.startswith("SET PROTOCOL SRCP "):
@@ -56,9 +66,9 @@ class Session:
                     self.reply("100 INFO 8 DESCRIPTION FB DESCRIPTION")
                     self.reply("100 INFO 8 FB 1 1")      # point-detector #1, value 1
                     self.reply("100 INFO 8 FB 2 0")      # point-detector #2, value 0
-                    global _sessions
                     for session in _sessions:
-                        self.reply("101 INFO 0 SESSION %d %s" % (session.session_id, session.mode))
+                        if session is not None:
+                            self.reply("101 INFO 0 SESSION %d %s" % (session.session_id, session.mode))
             elif c == "GET 6 DESCRIPTION":
                 self.reply("100 INFO 6 GA 1 0 0")    # turnout #0, port 0 value 0
                 self.reply("100 INFO 6 GA 1 1 0")    # turnout #0, port 1 value 0
@@ -75,18 +85,38 @@ class Session:
             elif c.startswith("TERM 0 SESSION"):
                 # client asked to close this connection
                 self.reply("200 OK")
-                conn.close()
+                return
+            elif c.startswith("TERM 0 SERVER"):
+                # client asked to close this server
+                self.reply("200 OK")
+                global _continue
+                _continue = False
+                global server
+                server.shutdown()
                 return
             else:
                 self.reply("200 OK")
                 #--reply("400 What?")
 
+    def finish(self):
+        print "Remove session", self.session_id
+        _sessions[self.session_id - 1] = None
 
-while True:
-    conn, address = s.accept()
-    session = Session(conn)
-    _sessions.append(session)
-    session.session_id = len(_sessions)
-    session.handle()
 
-s.close()
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+server = ThreadedTCPServer( ("localhost", 4303), SessionRequestHandler)
+server_thread = threading.Thread( target=server.serve_forever )
+server_thread.daemon = True
+server_thread.start()
+
+_c = 0
+while _continue:
+    if _c == 0:
+        print "Server loop running in thread:", server_thread.name
+    _c += 1
+    if _c == 60: _c = 0
+    time.sleep(1) # seconds
+
+server.shutdown()
