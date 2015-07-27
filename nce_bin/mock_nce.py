e394d4ebc249291615047c83c5c111db36164260
ralf <ralfoide@gmail.com>
2015-07-27 11:17:16 -0400

    JMRI: figure out how to use turnout feedback

--- nce_bin/mock_nce.py
+++ nce_bin/mock_nce.py
@@ -16,6 +16,8 @@ print 'Connected by', addr
 buf_len = 5
 buf = bytearray(buf_len)
 
+last_sensors = 0
+
 while True:
     n = conn.recv_into(buf, buf_len)
     if n == 0:
@@ -27,10 +29,15 @@ while True:
         # reply with version 6.3.8
         print "Version"
         conn.send("060308".decode("hex"))
+    
     elif n == 2 and c == 0x8A:
         # AIU polling, returns 4 bytes (sensor BE, mask BE, 14 bits max)
         print "Poll AIU", buf[1]
-        conn.send("3FFF0001".decode("hex"))
+        sensors = int(time.time() / 2) & 0x0FFFF
+        mask = sensors ^ last_sensors
+        last_sensors = sensors
+        conn.send( ("%04x%04x" % (sensors, mask)).decode("hex"))
+    
     elif n == 4 and c == 0xAD:
         # triggering turnouts
         print "Trigger Acc"
@@ -41,11 +48,21 @@ while True:
         elif op == 4:
             print "ACC reverse direction/OFF", addr
         conn.send("!")
+    
     elif n == 3 and c == 0x9D:
         # read one byte from RAM
         addr = (buf[1] << 8) + buf[2]
         print "Read RAM at", addr, "(0x%04x)" % addr
         conn.send("00".decode("hex"))
+    
+    elif n == 3 and c == 0x8F:
+        # read 16-bytes from the address specified
+        addr = (buf[1] << 8) + buf[2]
+        print "Read 16-bytes at", addr, "(0x%04x)" % addr
+
+        turnout = int(time.time()) & 0x0FF
+        conn.send( ("%02x" % turnout + "0"*30).decode("hex"))
+
     elif n == 2 and c == ord("T"):
         # Test
         # reply with version 1.2.3
