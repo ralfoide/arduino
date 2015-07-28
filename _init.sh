d640fb9ba29854bcdabacf5af2732fa69ccd50ca
ralf <ralfoide@gmail.com>
2015-07-27 19:31:01 -0700

    Reorganize all LayoutWifi projects together.

--- _init.sh
+++ _init.sh
@@ -19,5 +19,5 @@ function checkout() {
 
 checkout DigiFi https://github.com/digistump/DigiFi.git
 
-checkout srcp/microSRCP https://github.com/mc-b/microSRCP.git
+# checkout layout_wifi/microSRCP https://github.com/mc-b/microSRCP.git
 
