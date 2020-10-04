5ee5e7482e049218aa0a1201971bef0f75f67de0
ralf <ralfoide@gmail.com>
2020-10-03 17:57:05 -0700

    ESP32-CAM Crossing: VSCode setup, read SSID+pass from SD Card config file.

--- ESP32-CAM/GradeCrossingVision/GradeCrossingVision.ino
+++ ESP32-CAM/GradeCrossingVision/GradeCrossingVision.ino
@@ -1,4 +1,6 @@
 #include "esp_camera.h"
+#include <FS.h>
+#include <SD_MMC.h>
 #include <WiFi.h>
 
 // Set camera model
@@ -9,6 +11,9 @@
 #define PIN_FLASH 4
 #define PIN_LED 33
 
+
+// ==== Utilities ====
+
 void _blink() {
   digitalWrite(PIN_LED, LOW);
   delay(100 /*ms*/);
@@ -16,10 +21,45 @@ void _blink() {
 }
 
 void _flash(int durationMs) {
-  digitalWrite(PIN_FLASH, HIGH);
-  delay(durationMs /*ms*/);
+  if (durationMs > 0) {
+    digitalWrite(PIN_FLASH, HIGH);
+    delay(durationMs /*ms*/);
+  }
   digitalWrite(PIN_FLASH, LOW);
 }
+
+// ==== SD Card ====
+
+void _sd_init() {
+  if (!SD_MMC.begin()) {
+    Serial.println("SD: Card not found");
+  }
+}
+
+void _sd_read_config() {
+  uint8_t card_type = SD_MMC.cardType();
+  if (card_type == CARD_NONE) {
+    return;
+  }
+  File f = SD_MMC.open("/config.txt");
+  if (!f) {
+    Serial.println("SD: Config file not found.");
+    return;
+  }
+  String ssid = f.readStringUntil('\n');
+  String pass = f.readStringUntil('\n');
+  Serial.print("Wifi ssid: ");
+  Serial.println(ssid);
+  Serial.print("Wifi pass: ");
+  Serial.println(pass);
+  f.close();
+
+  // Ensure flash LED is off after reading SD MMC
+  pinMode(PIN_FLASH, OUTPUT);
+  _flash(0);
+}
+
+// ==== Setup/Loop ====
 
 void setup() {
   Serial.begin(115200);
@@ -31,14 +71,14 @@ void setup() {
   pinMode(PIN_LED, OUTPUT);
 
   _blink();
-  _flash(100);
-  _blink();
+  _sd_init();
+  _sd_read_config();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   delay(1000);
-  Serial.print("Loop");
+  Serial.println("Loop");
   _blink();
 }
 
