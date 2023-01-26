2567c22c8385d22000425832a37e964792ea572e
ralf <ralfoide@gmail.com>
2023-01-26 00:08:24 -0800

    ESP32: Rust support files.

---
+++ ESP32/rust_test1/export-esp.ps1
@@ -0,0 +1,1 @@
+/cygdrive/C/Users/ralf/export-esp.ps1
---
+++ ESP32/rust_test1/export-esp.sh
@@ -0,0 +1,7 @@
+PATH="$PATH:"$(cygpath "C:\Users\ralf\.espressif\tools\xtensa-esp32s2-elf\esp-2021r2-patch5-8_4_0\xtensa-esp32s2-elf\bin")
+PATH="$PATH:"$(cygpath "C:\Users\ralf\.espressif\tools\xtensa-esp32s3-elf\esp-2021r2-patch5-8_4_0\xtensa-esp32s3-elf\bin")
+PATH="$PATH:"$(cygpath "C:\Users\ralf\.espressif\tools\xtensa-esp32-elf\esp-2021r2-patch5-8_4_0\xtensa-esp32-elf\bin")
+PATH="$PATH:"$(cygpath "C:\Users\ralf\.espressif\tools\riscv32-esp-elf\esp-2021r2-patch5-8_4_0\riscv32-esp-elf\bin")
+PATH="$PATH:"$(cygpath  "C:\Users\ralf\.espressif\tools\xtensa-esp32-elf-clang\esp-15.0.0-20221201-x86_64-pc-windows-msvc\esp-clang\bin\libclang.dll")
+PATH="$PATH:"$(cygpath "C:\Users\ralf\.espressif\tools\xtensa-esp32-elf-clang\esp-15.0.0-20221201-x86_64-pc-windows-msvc\esp-clang\bin")
+echo "PATH=$PATH"
