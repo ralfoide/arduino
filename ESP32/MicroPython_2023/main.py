79e9a0bcabbffbf25ac5a0d6b9cb191ec49ab0df
ralf <ralfoide@gmail.com>
2023-01-29 17:50:51 -0800

    ESP32: MicroPython, dual thread blinky led.

--- ESP32/MicroPython_2023/main.py
+++ ESP32/MicroPython_2023/main.py
@@ -1,5 +1,8 @@
+import esp
+import machine
 from machine import Pin
 import time
+import _thread
 
 led_pin1 = Pin(25, Pin.OUT)
 led_pin2 = Pin(21, Pin.OUT)
@@ -13,19 +16,27 @@ def blink(pin):
     time.sleep_ms(250)
     pin.off()
 
+def thread_led(pin):
+    print("Thread LED", pin)
+    while True:
+        blink(pin)
+        time.sleep_ms(1000)
+
 def main_loop():
+    t_led1 = _thread.start_new_thread(thread_led, (led_pin1, ))
+    time.sleep_ms(500)
+    t_led2 = _thread.start_new_thread(thread_led, (led_pin2, ))
+    time.sleep_ms(250)
     n = 0
     while True:
-        blink(led_pin1)
-        time.sleep(1) # 1 sec
-
-        blink(led_pin2)
-        time.sleep(1) # 1 sec
-
         n += 1
-        print("blink",n)
+        time.sleep_ms(1000)
+        print("loop",n)
 
 
 print("Main is ", __name__)
 if __name__ == "__main__":
+    print("CPU freq:", machine.freq() / 1000 / 1000, "MHz")
+    print("Flash:", esp.flash_size() / 1024 / 1024, "MB")
+    print("Start:", esp.flash_user_start() / 1024 / 1024, "MB")
     main_loop()
