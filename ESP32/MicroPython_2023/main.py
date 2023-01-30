42271975660fb30d926b1e0edb1c12879df4adb8
ralf <ralfoide@gmail.com>
2023-01-29 18:30:26 -0800

    ESP32: MicroPython, ssd1306 OLED example.

--- ESP32/MicroPython_2023/main.py
+++ ESP32/MicroPython_2023/main.py
@@ -1,6 +1,7 @@
 import esp
 import machine
-from machine import Pin
+from machine import Pin, SoftI2C
+import ssd1306
 import time
 import _thread
 
@@ -22,6 +23,26 @@ def thread_led(pin):
         blink(pin)
         time.sleep_ms(1000)
 
+def init_oled():
+    global oled
+    rst = Pin(16, Pin.OUT)
+    rst.value(1)
+    scl = Pin(15, Pin.OUT, Pin.PULL_UP)
+    sda = Pin(4, Pin.OUT, Pin.PULL_UP)
+    i2c = SoftI2C(scl=scl, sda=sda, freq=450000)
+    oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
+
+oled_y_offset = 0
+oled_msg = "MicroPython"
+def update_oled(msg = None):
+    global oled_y_offset, oled_msg
+    if msg: oled_msg = msg
+    oled.fill(0)
+    oled.text("ESP32", 45, 5 + oled_y_offset)
+    oled.text(oled_msg, 20, 20 + oled_y_offset) 
+    oled.show()
+    oled_y_offset = (oled_y_offset + 1) % (64 - 8)
+
 def main_loop():
     t_led1 = _thread.start_new_thread(thread_led, (led_pin1, ))
     time.sleep_ms(500)
@@ -29,6 +50,7 @@ def main_loop():
     time.sleep_ms(250)
     n = 0
     while True:
+        update_oled()
         n += 1
         time.sleep_ms(1000)
         print("loop",n)
@@ -39,4 +61,5 @@ if __name__ == "__main__":
     print("CPU freq:", machine.freq() / 1000 / 1000, "MHz")
     print("Flash:", esp.flash_size() / 1024 / 1024, "MB")
     print("Start:", esp.flash_user_start() / 1024 / 1024, "MB")
+    init_oled()
     main_loop()
