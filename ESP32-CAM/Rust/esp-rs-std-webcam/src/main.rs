1e26db816c3dae94eadc978e8469b54ecdebb776
ralf <ralfoide@gmail.com>
2025-10-05 01:16:09 -0700

    ESP32-CAM: ESP-RS std, task/thread stack size seems wrong.

--- ESP32-CAM/Rust/esp-rs-std-webcam/src/main.rs
+++ ESP32-CAM/Rust/esp-rs-std-webcam/src/main.rs
@@ -1,11 +1,12 @@
 use std::pin::pin;
+use std::ptr;
 use esp_idf_hal::cpu;
 use esp_idf_hal::cpu::Core;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::task::block_on;
 use esp_idf_hal::task::thread::ThreadSpawnConfiguration;
 use esp_idf_svc::eventloop::{EspEventLoop, EspSystemEventLoop, System};
-use esp_idf_sys::{heap_caps_get_free_size, MALLOC_CAP_8BIT, MALLOC_CAP_INTERNAL, MALLOC_CAP_SPIRAM};
+use esp_idf_sys::{heap_caps_get_free_size, uxTaskGetStackHighWaterMark2, xTaskGetCurrentTaskHandle, MALLOC_CAP_8BIT, MALLOC_CAP_INTERNAL, MALLOC_CAP_SPIRAM};
 use esp_rs_std_webcam::board::Board;
 use esp_rs_std_webcam::task_camera::CameraCountEvent;
 
@@ -29,6 +30,10 @@ fn main() -> anyhow::Result<()> {
     log::info!("@@ Free 8-bit RAM: {} bytes", free_ram_bytes);
     free_ram_bytes = unsafe { heap_caps_get_free_size(MALLOC_CAP_SPIRAM) };
     log::info!("@@ Free external RAM: {} bytes", free_ram_bytes);
+    let stack_high_water_mark = unsafe { uxTaskGetStackHighWaterMark2(ptr::null_mut()) };
+    log::info!("@@ Stack High Water Mark: {} words", stack_high_water_mark);
+    let task_handle = unsafe { xTaskGetCurrentTaskHandle() };
+    log::info!("@@ Task Handle: {:?}", task_handle);
 
     let sys_loop = EspSystemEventLoop::take()?;
     let sys_loop_cam = sys_loop.clone();
@@ -39,18 +44,21 @@ fn main() -> anyhow::Result<()> {
     // Configure threads to have an affinity on Core 1.
     create_thread("cam_task\0", 4096, 1, Core::Core1)
         .spawn(|| {
+            log_task_info("CAM");
             // esp_rs_std_webcam::task_camera::run_camera(Board::get(), sys_loop_cam)
         })?;
 
-    create_thread("led_task\0", 4096, 1, Core::Core1)
+    create_thread("led_task\0", 4096, 2, Core::Core1)
         .spawn(|| {
+            log_task_info("LED");
             // esp_rs_std_webcam::task_led::run_led(Board::get())
         })?;
 
-    // create_thread("wifi_task\0", 4096*4, 1, Core::Core0)
-    //     .spawn(|| {
-    //         esp_rs_std_webcam::task_wifi::run_wifi(Board::get(), sys_loop_wifi)
-        // })?;
+    create_thread("wifi_task\0", 4096*2, 3, Core::Core0)
+        .spawn(|| {
+            log_task_info("WIFI");
+            // esp_rs_std_webcam::task_wifi::run_wifi(Board::get(), sys_loop_wifi)
+        })?;
     let res = esp_rs_std_webcam::task_wifi::run_wifi(Board::get(), sys_loop_wifi);
     log::info!("@@ [WIFI RETURNED] {:?}", res);
 
@@ -75,22 +83,34 @@ fn create_thread(name: &'static str, stack_size: usize, priority: u8 /* 1..24 */
      .unwrap();
 
     std::thread::Builder::new()
+        .stack_size(stack_size)
+}
+
+fn log_task_info(name: &str) {
+    let task_handle = unsafe { xTaskGetCurrentTaskHandle() };
+    let stack_high_water_mark = unsafe { uxTaskGetStackHighWaterMark2(task_handle) };
+    let core_id: i32 = cpu::core().into();
+    log::info!("@@ [{}] Task Handle: {:?} -- Core {} -- Stack high mark: {} bytes",
+        name,
+        task_handle,
+        core_id,
+        stack_high_water_mark * 4);
 }
 
 async fn task_core0(sys_loop: EspEventLoop<System>) -> anyhow::Result<()> {
-    let core_id: i32 = cpu::core().into();
-    log::info!("@@ Task 0.. running on Core #{}", core_id);
-
     // RM: Note that subscribe_async _forces_ the recv().await call below to be blocking.
     // For a non-blocking version, use subscribe() with a closure callback that emits a signal.
     let mut subscription = sys_loop.subscribe_async::<CameraCountEvent>()?;
 
     loop {
+        let stack_high_water_mark = unsafe { uxTaskGetStackHighWaterMark2(ptr::null_mut()) };
+        let core_id: i32 = cpu::core().into();
+        log::info!("@@ Task 0.. running on Core #{}", core_id);
+        log::info!("@@ [TASK 0] Stack High Water Mark: {} bytes", stack_high_water_mark * 4);
+
         let event = subscription.recv().await?;
         log::info!("@@ Task 0 event recv: {:?}", event);
 
-        let core_id: i32 = cpu::core().into();
-        log::info!("@@ Task 0.. running on Core #{}", core_id);
         FreeRtos::delay_ms(100);
     }
 }
--- ESP32-CAM/Rust/esp-rs-std-webcam/src/task_wifi.rs
+++ ESP32-CAM/Rust/esp-rs-std-webcam/src/task_wifi.rs
@@ -1,3 +1,4 @@
+use std::ptr;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicI32, Ordering};
 use esp_idf_hal::cpu;
@@ -5,14 +6,19 @@ use esp_idf_hal::delay::FreeRtos;
 use esp_idf_svc::eventloop::{EspEventLoop, System};
 use esp_idf_svc::http::Method;
 use esp_idf_svc::http::server::{EspHttpConnection, EspHttpServer, Request};
-use esp_idf_svc::wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi};
+use esp_idf_svc::wifi::{AccessPointConfiguration, AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi};
+use esp_idf_sys::uxTaskGetStackHighWaterMark2;
 use crate::board::Board;
 use crate::wifi_info;
 
-const HTTPD_STACK_SIZE: usize = 4096*4; // minimum is 6656
-
+const HTTPD_STACK_SIZE: usize = 4096;
+const WIFI_AP_CHANNEL: u8 = 3;
+const WIFI_IS_AP: bool = true;
 
 pub fn run_wifi(board: &'static Board, sys_loop: EspEventLoop<System>) -> anyhow::Result<()> {
+    let stack_high_water_mark = unsafe { uxTaskGetStackHighWaterMark2(ptr::null_mut()) };
+    log::info!("@@ [WIFI] Stack High Water Mark: {} bytes", stack_high_water_mark * 4);
+
     let modem_mutex = &board.modem.get().unwrap();
     let modem = modem_mutex.lock().unwrap();
 
@@ -40,7 +46,9 @@ pub fn run_wifi(board: &'static Board, sys_loop: EspEventLoop<System>) -> anyhow
     loop {
         let core_id: i32 = cpu::core().into();
         let counter = req_count.load(Ordering::Relaxed);
-        log::info!("@@ Task WIFI loop.. running on Core #{}... {} requests", core_id, counter);
+        let stack_high_water_mark = unsafe { uxTaskGetStackHighWaterMark2(ptr::null_mut()) };
+        log::info!("@@ [WIFI] Stack High Water Mark: {} bytes", stack_high_water_mark * 4);
+        log::info!("@@ [WIFI] loop.. running on Core #{}... {} requests", core_id, counter);
         FreeRtos::delay_ms(1000);
     }
 }
@@ -55,25 +63,27 @@ fn handle_req(req: Request<&mut EspHttpConnection>, req_count: &Arc<AtomicI32>) 
 fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
     log::info!("@@ [WIFI] Configure, then wait for connection");
 
-    // For the AP (adhoc wifi) case:
-    // let wifi_configuration = wifi::Configuration::AccessPoint(AccessPointConfiguration {
-    //     ssid: SSID.try_into().unwrap(),
-    //     ssid_hidden: true,
-    //     auth_method: AuthMethod::WPA2Personal,
-    //     password: PASSWORD.try_into().unwrap(),
-    //     channel: CHANNEL,
-    //     ..Default::default()
-    // });
-
-    // To join an existing Access Point:
-    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
-        ssid: wifi_info::WIFI_SSID.try_into().unwrap(),
-        bssid: None,
-        auth_method: AuthMethod::WPA2Personal,
-        password: wifi_info::WIFI_PASSWD.try_into().unwrap(),
-        channel: None,
-        ..Default::default()
-    });
+    let wifi_configuration: Configuration = if (WIFI_IS_AP) {
+        // For the AP (adhoc wifi) case:
+        Configuration::AccessPoint(AccessPointConfiguration {
+            ssid: "AP_WIFI_SSID".try_into().unwrap(),
+            ssid_hidden: true,
+            auth_method: AuthMethod::WPA2Personal,
+            password: wifi_info::WIFI_PASSWD.try_into().unwrap(),
+            channel: WIFI_AP_CHANNEL,
+            ..Default::default()
+        })
+    } else {
+        // To join an existing Access Point:
+        Configuration::Client(ClientConfiguration {
+            ssid: wifi_info::WIFI_SSID.try_into().unwrap(),
+            bssid: None,
+            auth_method: AuthMethod::WPA2Personal,
+            password: wifi_info::WIFI_PASSWD.try_into().unwrap(),
+            channel: None,
+            ..Default::default()
+        })
+    };
 
     wifi.set_configuration(&wifi_configuration)?;
 
@@ -84,7 +94,11 @@ fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()>
     loop {
         let res = wifi.wait_netif_up();
         if let Ok(_value) = res {
-            let ip = wifi.wifi().sta_netif().get_ip_info();
+            let ip = if (WIFI_IS_AP) {
+                wifi.wifi().ap_netif().get_ip_info()
+            } else {
+                wifi.wifi().sta_netif().get_ip_info()
+            };
             log::info!("@@ [WIFI] netif UP OK, IP: {:?}", ip);
             break;
         } else {
